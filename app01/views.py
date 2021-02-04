2a7a41a1c7a9d355eee96eb51aca6b879f05ea2d
xinxinliang <liangxinxin5102@163.com>
2021-02-04 10:53:12 +0800

    master

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def homepage(request):
+    post = Post.object.all()
+    post_list = post()
+    for count,post in enumerate(post):
+        post_list.append("No.{}".format(str(count)+str(post)+'<br>'))
+    return HttpResponse(post_list)
--- djangoDemo/urls.py
+++ djangoDemo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/',homepage),
 ]
